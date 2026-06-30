e7753f5cfad7649ffbff57486bef04325256ae5c
Eglor <leontevegor57@gmail.com>
2026-06-30 17:12:14 +0200

    -

--- frontend/src/app/track/page.tsx
+++ frontend/src/app/track/page.tsx
@@ -1,12 +1,70 @@
-export default function Tes() {
-  const age = 19;
+'use client';
 
-  function getYear() {
-    if (age < 15) {
-      console.log(`В наше заведение нельзя людям с возрастом ${age}`);
-    } else {
-      console.log('Проходите');
-    }
+import { useEffect, useState } from 'react';
+interface IUser {
+  name: string;
+  age: number;
+  prof: string;
+  course: number;
+  isAdmin: boolean;
+  nameColor: string | null;
+}
+
+export default function Tes() {
+  const [theme, setTheme] = useState<boolean>(true);
+  const [color, setColor] = useState('f1f1f1');
+  const [inputValue, setInputValue] = useState('');
+
+  const switchColor = () => {
+    setColor(inputValue);
+  };
+
+  switch (key) {
+    case value:
+      break;
+
+    default:
+      break;
   }
-  return <div className=""></div>;
+
+  const user: IUser = {
+    name: 'Egor',
+    age: 19,
+    prof: 'software ingener',
+    course: 2,
+    isAdmin: true,
+    nameColor: color,
+  };
+
+  function switchTheme() {
+    setTheme((prev) => !prev);
+  }
+
+  return (
+    <div className={`flex w-full h-[100vh] ${theme == false ? 'bg-black' : 'bg-white'}`}>
+      <p className="w-full h-full" onClick={switchTheme}>
+        HEllo
+      </p>
+
+      <button onClick={switchTheme}>Переключить тему</button>
+
+      <input
+        type="text"
+        value={inputValue}
+        className="bg-gray-200"
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={switchColor}>Применить цвет</button>
+      <p className="">
+        Ваше имя:{' '}
+        <span
+          style={{
+            color: `#${user.nameColor}`,
+          }}>
+          {user.name}
+        </span>
+      </p>
+      <p className="">{user.isAdmin ? 'Admin' : 'Not a admin'}</p>
+    </div>
+  );
 }
